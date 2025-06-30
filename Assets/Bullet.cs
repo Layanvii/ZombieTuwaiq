4221264d88866530bb4e69bb4273aed9f9bbee1d
Layanvii <layanvii7@gmail.com>
2025-06-30 18:56:26 +0300

    update gun

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,21 +1,12 @@
+
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float lifetime = 3f;
-
     void Start()
     {
-        Destroy(gameObject, lifetime); //  Œ ›Ì »⁄œ Êﬁ 
+        // Õ–› «·ÿ·ﬁ… »⁄œ 5 ÀÊ«‰Ì  ·ﬁ«∆Ì«
+        Destroy(gameObject, 2);
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            Destroy(collision.gameObject); // Õ–› «·“Ê„»Ì
-        }
-
-        Destroy(gameObject); // Õ–› «·ÿ·ﬁ… ‰›”Â«
-    }
 }
